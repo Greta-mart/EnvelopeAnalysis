f1669ea565ba7a804725a17b3e2725ef7a500db2
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-01 14:33:39 +0300

    add correction

--- src/EnvelopeAnalysis/Envelope.java
+++ src/EnvelopeAnalysis/Envelope.java
@@ -3,7 +3,11 @@ package EnvelopeAnalysis;
 public class Envelope {
     float a, b;
 
-    public boolean compareEnvelope (){
+    Envelope(float a, float b){
+        a = this.a;
+        b = this.b;
+    }
+    public boolean compareEnvelope (float c, float d){
         if (a >= c && b >= d) {
             return true;
         }
