16a23f60b600670dd537dbf1588d43824f54e73c
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-01 15:16:29 +0300

    fix

--- src/EnvelopeAnalysis/Main.java
+++ src/EnvelopeAnalysis/Main.java
@@ -2,10 +2,12 @@ package EnvelopeAnalysis;
 
 public class Main {
     public static void main(String[] args) {
-        Analysis analysis = new Analysis();
+        float a=0, b=0, c=0, d=0;
 
-        analysis.inputData();
-        analysis.analysisData();
-        analysis.continueWork();
+        Application app = new Application(a, b);
+        app.Run();
+        Envelope env1 = new Envelope(a, b);
+        Envelope env2 = new Envelope(c ,d);
+        env1.compareEnvelope(env2);
     }
 }
