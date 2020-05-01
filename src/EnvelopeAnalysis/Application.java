277807b4ef1a9d9cb6fcb437a0cc45d3edcea127
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-01 15:26:45 +0300

    fix

--- src/EnvelopeAnalysis/Application.java
+++ src/EnvelopeAnalysis/Application.java
@@ -4,24 +4,37 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application{
+    float a, b, c, d;
 
-    public void Run(float width, float length) {
+    public void Run() {
 
         Scanner s = new Scanner(System.in);
-
-        for (int i = 0; i <= 1; i++) {
             try {
-                System.out.print("Please enter the length of envelope " + i +": ");
-                width = s.nextFloat();
+                System.out.print("Please enter the length of envelope [1]: ");
+                a = s.nextFloat();
 
-                System.out.print("Please enter the width of envelope " + i +": ");
-                length = s.nextFloat();
+                System.out.print("Please enter the width of envelope [1]: ");
+                b = s.nextFloat();
+
+                System.out.print("Please enter the length of envelope [2]: ");
+                c = s.nextFloat();
+
+                System.out.print("Please enter the width of envelope  [2]: ");
+                d = s.nextFloat();
 
             } catch (InputMismatchException ex) {
                 System.out.println("The length and width of envelopes should be numbers");
                 System.exit(1);
             }
-        }
+            Envelope env1 = new Envelope(a, b);
+            Envelope env2 = new Envelope(c, d);
+            if (env1.compareEnvelope(env2)){
+                System.out.println("The envelope [2] can be placed into the envelop [1]");
+            }
+            else {
+                System.out.println("The envelope [2] can't be placed into the envelop [1]");
+            }
+
     }
 
 }
--- src/EnvelopeAnalysis/Main.java
+++ src/EnvelopeAnalysis/Main.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args) {
         float a=0, b=0, c=0, d=0;
 
-        Application app = new Application(a, b);
+        Application app = new Application();
         app.Run();
-        Envelope env1 = new Envelope(a, b);
-        Envelope env2 = new Envelope(c ,d);
-        env1.compareEnvelope(env2);
+
     }
 }
