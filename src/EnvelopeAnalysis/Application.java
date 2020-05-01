ed7772fc4e6c26264d71934cb47dc52bde74f588
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-01 16:00:29 +0300

    fix

--- src/EnvelopeAnalysis/Application.java
+++ src/EnvelopeAnalysis/Application.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 
 public class Application{
 
-
-
     public void Run() {
         float a = 0, b = 0, c = 0, d = 0;
+
+        Envelope env1 = new Envelope(a, b);
+        env1.length = a;
+        env1.width = b;
+        Envelope env2 = new Envelope(c, d);
+        env2.length = c;
+        env2.width = d;
 
         Scanner s = new Scanner(System.in);
             do {
@@ -24,27 +29,19 @@ public class Application{
 
                     System.out.print("Please enter the width of envelope  [2]: ");
                     d = s.nextFloat();
-
                 } catch (InputMismatchException ex) {
                     System.out.println("The length and width of envelopes should be numbers");
                     System.exit(1);
                 }
-                Envelope env1 = new Envelope(a, b);
-                Envelope env2 = new Envelope(c, d);
                 if (env1.compareEnvelope(env2)) {
                     System.out.println("The envelope [2] can be placed into the envelop [1]");
                 } else {
                     System.out.println("The envelope [2] can't be placed into the envelop [1]");
                 }
-                s.close(); /*Please enter the length of envelope [1]: Exception in thread "main" java.lang.IllegalStateException: Scanner closed
-                at java.base/java.util.Scanner.ensureOpen(Scanner.java:1150)
-                at java.base/java.util.Scanner.next(Scanner.java:1573)
-                at java.base/java.util.Scanner.nextFloat(Scanner.java:2496)
-                at EnvelopeAnalysis.Application.Run(Application.java:17)
-                at EnvelopeAnalysis.Main.main(Main.java:8)
-это если закрыть сканер(((( нужно будет подумтаь куда его вынести*/
             }
+
             while (true);
+       // s.close();
     }
 
 }
--- src/EnvelopeAnalysis/Envelope.java
+++ src/EnvelopeAnalysis/Envelope.java
@@ -8,13 +8,6 @@ public class Envelope {
         this.length = b;
     }
 
-    public void setWidth(float width) {
-        this.width = width;
-    }
-
-    public void setLength(float length) {
-        this.length = length;
-    }
     public boolean compareEnvelope (Envelope obj){
         return width > obj.width && length > obj.length;
     }
