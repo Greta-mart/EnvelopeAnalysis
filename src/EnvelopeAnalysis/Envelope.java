0e517cdb812173f9ef603e1b70bb450e431542ab
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-01 15:53:06 +0300

    fix

--- src/EnvelopeAnalysis/Envelope.java
+++ src/EnvelopeAnalysis/Envelope.java
@@ -7,6 +7,14 @@ public class Envelope {
         this.width = a;
         this.length = b;
     }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
     public boolean compareEnvelope (Envelope obj){
         return width > obj.width && length > obj.length;
     }
